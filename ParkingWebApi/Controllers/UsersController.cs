5c997407ef0ab9a8d0e2aab4dfbadb7a395c44fb
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2024-11-13 21:37:36 +0600

    Контроллеры

---
+++ ParkingWebApi/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkingWebApi.Data;
+using ParkingWebApi.Models.Dto;
+using ParkingWebApi.Models.Enteties;
+
+namespace ParkingWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(UserDto dto)
+        {
+            var user = new User()
+            {
+                Id = new Guid(),
+                Name = dto.Name
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("PostUser", user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
