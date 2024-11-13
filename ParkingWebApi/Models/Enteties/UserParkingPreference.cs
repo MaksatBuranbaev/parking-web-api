dfae8c4929cf4f2c46ba777d5b449b31ffc00325
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2024-11-13 21:40:06 +0600

    Модель

---
+++ ParkingWebApi/Models/Enteties/UserParkingPreference.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParkingWebApi.Models.Enteties
+{
+    public class UserParkingPreference
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+        public Guid ParkingSpaceId { get; set; }
+        public ParkingSpace ParkingSpace { get; set; } = null!;
+        public int Preference_Level { get; set; }
+    }
+}
