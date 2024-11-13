ee5f719a5a7069e120f5a4621da40126ed1e9afd
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2024-11-13 21:37:23 +0600

    Dto

---
+++ ParkingWebApi/Models/Dto/ParkingSpaceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ParkingWebApi.Models.Dto
+{
+    public class ParkingSpaceDto
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = string.Empty;
+    }
+}
